d319b1fdb51d9843456ef55a78d1f4b5caf956d0
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-09-07 18:01:37 +0200

    Refactor & sum of random amount of ints covered.

--- src/test/java/com/codurance/StringCalculatorShould.java
+++ src/test/java/com/codurance/StringCalculatorShould.java
@@ -1,6 +1,5 @@
 package com.codurance;
 
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,8 +8,8 @@ class StringCalculatorShould {
 
     @Test
     void return_zero_when_given_an_empty_string() {
-        int sum = StringCalculator.add("");
-        assertEquals(0, sum);
+        int numericRepresentation = StringCalculator.add("");
+        assertEquals(0, numericRepresentation);
     }
 
     @Test
@@ -20,8 +19,14 @@ class StringCalculatorShould {
     }
 
     @Test
-    void return_sum_of_a_comma_separated_numbers() {
+    void return_sum_of_two_comma_separated_numbers() {
         int numericRepresentation = StringCalculator.add("1,2");
         assertEquals(3, numericRepresentation);
+    }
+
+    @Test
+    void return_sum_of_any_amount_of_comma_separated_numbers() {
+        assertEquals(6, StringCalculator.add("1,2,3"));
+        assertEquals(5, StringCalculator.add("1,1,1,1,1"));
     }
 }
